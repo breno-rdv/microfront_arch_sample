d39c56e658eb3696b18033ebec8238c90ba098e0
Breno do Val <brenorvale@gmail.com>
2024-01-18 19:20:54 -0300

    feat(CheckoutApp): add style to Checkout

--- checkout_app/src/App.js
+++ checkout_app/src/App.js
@@ -3,7 +3,16 @@ import './styles.css';
 
 function App() {
     return (
-        <div className='container'>Checkout</div>
+        <div className='container'>
+            <div className='cart'>
+                <div className='cart-title'>
+                    <span class="material-symbols-outlined">shopping_cart</span>
+                    <div>Check-out</div>
+                </div>
+                <div className='value'>Value</div>
+                <button>Finalizar compra</button>
+            </div>
+        </div>
     );
 }
 
