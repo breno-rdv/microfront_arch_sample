78e2cf80f2ee1e28bfc3ec18c2140d0f270bf6b0
Breno do Val <brenorvale@gmail.com>
2024-01-24 22:43:32 -0300

    feat(MainApp): add logic to clear cart after concluding porder

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useLocation } from 'react-router-dom';
+import { Route, Switch, Link, useLocation } from 'react-router-dom';
 import CriptoList from './pages/CriptoList/CriptoList';
+import checkout from './cart-observable';
 import './styles.css';
 
 const Header =  React.lazy(() => import('header/Header'));
@@ -10,9 +11,13 @@ const Checkout = React.lazy(() => import('checkout/Checkout'));
 function App() {
     const [ total, setTotal ] = React.useState(0);
     const [ pathname, setPathname ] = React.useState(null);
-    let location = useLocation();
+    const location = useLocation();
 
     React.useEffect(() => {
+        if (location.search === '?page=checkout') {
+            setTotal(0);
+            checkout.postMessage('clearCart');
+        }
         setPathname(location.pathname);
     }, [location]);
 
