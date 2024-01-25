6fa0e457f7adc73c78559823de4dfd486059eff8
Breno do Val <brenorvale@gmail.com>
2024-01-24 22:05:13 -0300

    feat(MainApp): add logic to Checkout button

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, useLocation } from 'react-router-dom';
 import CriptoList from './pages/CriptoList/CriptoList';
 import './styles.css';
 
@@ -9,35 +9,42 @@ const Checkout = React.lazy(() => import('checkout/Checkout'));
 
 function App() {
     const [ total, setTotal ] = React.useState(0);
+    const [ pathname, setPathname ] = React.useState(null);
+    let location = useLocation();
+
+    React.useEffect(() => {
+        setPathname(location.pathname);
+    }, [location]);
 
     return (
-        <Router>
-            <main className='container'>
-                <header className='container'>
-                    <React.Suspense fallback={<h1>Loading....</h1>}>
-                        <Header />
-                    </React.Suspense>
-                </header>
-                <section className='body'>
-                    <Switch>
-                        <Route exact path="/">
-                            <CriptoList setTotal={setTotal}/>
-                        </Route>
-                        <Route path="/checkout">
-                            <React.Suspense fallback={<h1>Loading....</h1>}>
-                                <Checkout total={total}/>
-                            </React.Suspense>
-                        </Route>
-                    </Switch>
-                </section>
-                {/** TODO: Add logic to change footer based on route */}
-                <footer className='footer'>
+        <main className='container'>
+            <header className='container'>
+                <React.Suspense fallback={<h1>Loading....</h1>}>
+                    <Header />
+                </React.Suspense>
+            </header>
+            <section className='body'>
+                <Switch>
+                    <Route exact path="/">
+                        <CriptoList setTotal={setTotal}/>
+                    </Route>
+                    <Route path="/checkout">
+                        <React.Suspense fallback={<h1>Loading....</h1>}>
+                            <Checkout total={total}/>
+                        </React.Suspense>
+                    </Route>
+                </Switch>
+            </section>
+            <footer className='footer'>
+                { pathname === '/' ? (
                     <Link to="/checkout">
-                        <button>Check out</button>
+                        <button disabled={total === 0}>Check out</button>
                     </Link>
-                </footer>
-            </main>
-        </Router>
+                ) : (
+                    <div>Crypto App</div>
+                )}
+            </footer>
+        </main>
     );
 }
 
